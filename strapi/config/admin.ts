d1c3e964f072688decbc283cb026810eaae603af
Arthur Le_Goux <arthur.legoux@gmail.com>
2025-04-09 16:41:54 +0200

    add of strapi as a backend

--- strapi/config/admin.ts
+++ strapi/config/admin.ts
@@ -1,4 +1,6 @@
-export default ({ env }) => ({
+
+type Env = (key: string, defaultValue?: string | boolean | number) => string | boolean | number | undefined;
+export default ({ env }: { env: Env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
@@ -11,7 +13,7 @@ export default ({ env }) => ({
     },
   },
   flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+    nps: env('FLAG_NPS', true),
+    promoteEE: env('FLAG_PROMOTE_EE', true),
   },
 });
